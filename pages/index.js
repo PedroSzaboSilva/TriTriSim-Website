17648414160b65fdd2f73e7f025f69d6841cda2a
Pedro Szabo <pedrinszabo@gmail.com>
2025-07-20 20:22:43 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 TriTriSim Installer</h1>
+      <p>Hello World! Funciona demais 😎</p>
+    </div>
+  );
+}
