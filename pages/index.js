cb086d4eff81e596c0cf581bd64abfbc1af70dc4
Bofamily <familytbh6+githubbofamily@gmail.com>
2025-07-21 16:55:51 +0100

    added nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,45 @@
 export default function Home() {
   return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+    <html>    
+      <nav class="menu-container">
+
+  <input type="checkbox" aria-label="Toggle menu" />
+  <span></span>
+  <span></span>
+  <span></span>
+
+
+  <a href="https://tritrisim.com" class="menu-logo">
+    <img src="https://storage.ko-fi.com/cdn/useruploads/3c143159-e5b1-4937-be4d-a50a85fc00f3_66d56e99-00a0-45a5-851e-fd25252abcab.png" alt="Logo"/>
+  </a>
+
+
+  <div class="menu">
+    <ul>
+      <li>
+        <a href="https://tritrisim.com">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/TriTriTheCuber/TFX/raw/refs/heads/main/TriTriSim%20Installer.exe" target="_blank" rel="noopener noreferrer">
+          Download
+        </a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="https://ko-fi.com/tritrithecuber">
+          Donate
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
       <h1>🚀 TriTriSim Installer</h1>
       <p>Hello World! Funciona demais 😎</p>
     </div>
+    </html>
   );
 }
