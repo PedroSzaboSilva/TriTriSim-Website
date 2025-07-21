a75db1aeda9ff0b9192d14c34540412454557dfd
Bofamily <familytbh6+githubbofamily@gmail.com>
2025-07-21 19:58:04 +0100

    update (major)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 export default function Home() {
   return (
     <>
@@ -7,16 +6,19 @@ export default function Home() {
   overflow-x: hidden;
   font-family: Sans-Serif;
   margin: 0;
-}
+  {* background-image: url('https://installer.cdn.tritrisim.com/Images/FenixA319Fire.png');
+}*/}
+
+  
 
 .menu-container {
   position: relative;
   display: flex;
   align-items: center;
-  margin-bottom: 20px;
+  margin-bottom: 0px;
   background: #000000;
   color: #cdcdcd;
-  padding: 20px;
+  padding:20px;
   z-index: 1;
   -webkit-user-select: none;
   user-select: none;
@@ -110,7 +112,7 @@ export default function Home() {
     flex-direction: column;
     align-items: flex-end;
   }
-  
+
   .menu-logo {
     position: absolute;
     left: 0;
@@ -177,16 +179,50 @@ export default function Home() {
   .menu li {
     padding: 0 20px;
   }
+  
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+/* body {
+  margin: 0;
+  padding: 0;
+  background-image: url('https://installer.cdn.tritrisim.com/Images/FenixA319Fire.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  position: relative;
+  z-index: 0;
+}
+
+ body::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  opacity: 0.4;
+  pointer-events: none;
+  z-index: 0;
+} */
+
+
 }`}
       </style>
-      <meta name="google-site-verification" content="Vv1QCyqEuLW2jB0y5Ry2H8Ub-NDJGm4DU4mUw-rU890" />
       <nav className="menu-container">
         <input type="checkbox" aria-label="Toggle menu" />
         <span></span>
         <span></span>
         <span></span>
         <a href="https://tritrisim.com" className="menu-logo">
-          <img src="https://storage.ko-fi.com/cdn/useruploads/3c143159-e5b1-4937-be4d-a50a85fc00f3_66d56e99-00a0-45a5-851e-fd25252abcab.png" alt="Logo"/>
+          <img
+            src="https://storage.ko-fi.com/cdn/useruploads/3c143159-e5b1-4937-be4d-a50a85fc00f3_66d56e99-00a0-45a5-851e-fd25252abcab.png"
+            alt="Logo"
+          />
         </a>
         <div className="menu">
           <ul>
@@ -194,7 +230,11 @@ export default function Home() {
               <a href="https://tritrisim.com">Home</a>
             </li>
             <li>
-              <a href="https://github.com/TriTriTheCuber/TFX/raw/refs/heads/main/TriTriSim%20Installer.exe" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://github.com/TriTriTheCuber/TFX/raw/refs/heads/main/TriTriSim%20Installer.exe"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Download
               </a>
             </li>
@@ -206,9 +246,89 @@ export default function Home() {
           </ul>
         </div>
       </nav>
-      <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
-        <h1>🚀 TriTriSim Installer</h1>
-        <p>Hello World! Funciona demais 😎</p>
+
+      <section
+        style={{
+          backgroundImage:
+            "url('https://installer.cdn.tritrisim.com/Images/FenixA319Fire.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "90vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "black",
+            opacity: 0.6,
+            zIndex: 0,
+          }}
+        />
+
+        <a
+          href="https://github.com/TriTriTheCuber/TFX/raw/refs/heads/main/TriTriSim%20Installer.exe"
+          style={{
+            fontSize: "2rem",
+            padding: "15px 30px",
+            borderRadius: "12px",
+            maxWidth: "300px",
+            width: "80%",
+            textAlign: "center",
+            backgroundColor: "transparent",
+            border: "2px solid white",
+            color: "white",
+            textDecoration: "none",
+            cursor: "pointer",
+            zIndex: 1,
+            position: "relative",
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download
+        </a>
+      </section>
+
+      <div style={{ display: "grid", margin: "20px", fontFamily: 'Open Sans, system-ui, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, Noto Sans, Segoe UI, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji' }}>
+        <div
+          className="card"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div className="texts" style={{ fontSize: "100%" }}>
+            <h2 style={{ fontSize: "1.875rem" }}>TriTriSim (TFX)</h2>
+            <p>
+              TFX Is an addon for <b>Microsoft Flight Simulator</b> TFX Supports
+              both editions <b>(2020 and 2024).</b> TFX adds realistic looking
+              effects such as <b>Fire, Smoke, Sparks and crash physics</b>
+              <sup>
+                {" "}
+                (Crash physics are currently only avalible in experimental
+                section)
+              </sup>
+            </p>
+          </div>
+
+          <div className="image">
+            <img
+              src="https://installer.cdn.tritrisim.com/Images/777-3fire.png"
+              width="500rem"
+              style={{ borderRadius: "10px" }}
+            ></img>
+          </div>
+        </div>
       </div>
     </>
   );
