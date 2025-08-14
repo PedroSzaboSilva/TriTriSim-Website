b6633a5f5e9a5c8bf8b50ae75d1d4a30886e075f
gfyefrher <familytbh6+githubbofamily@gmail.com>
2025-08-14 12:50:11 +0100

    ok fixed water

--- pages/index.js
+++ pages/index.js
@@ -293,7 +293,7 @@ html {
               <a href="#requirements">Requirements</a>
             </li>
             <li>
-              <a href="/water/">Water</a>
+              <a href="/water">Water</a>
             </li>
             <li>
               <a href="https://github.com/TriTriTheCuber/TFX/raw/refs/heads/main/TriTriSim%20Installer.exe" target="_blank" rel="noopener noreferrer">
